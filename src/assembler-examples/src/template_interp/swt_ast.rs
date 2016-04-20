cb718dcf5ec8ae19ee025544cc48abae24a4b341
Overmind JIANG <p90eri@gmail.com>
2016-04-20 20:46:47 +0800

    Thinking about trace analysis...

--- src/assembler-examples/src/template_interp/swt_ast.rs
+++ src/assembler-examples/src/template_interp/swt_ast.rs
@@ -1,20 +1,23 @@
 use super::stack_with_trace::Op;
 
+#[derive(Eq, PartialEq, Debug)]
 pub enum Node {
     Add(Box<Node>, Box<Node>),
     ReadLocal(u8),
     LitI8(i8),
 }
 
+#[derive(Eq, PartialEq, Debug)]
 pub enum Stmt {
     BranchLt(Node, Node, i8 /* bc offset */, bool /* taken */),
+    Ret(Node, u8 /* nlocals */),
+    Seq(Vec<Stmt>),
 }
 
-pub fn trace_to_node(ops: &[*const u8]) -> Node {
+pub fn trace_to_node(ops: &[*const u8]) -> Stmt {
     let mut i = 0;
     let mut stack = vec![];
     let mut stmts = vec![];
-    let res;
 
     fn next_op(ops: &[*const u8], i: &mut usize) -> Op {
         let op = Op::from(unsafe { *ops[*i] });
@@ -36,17 +39,23 @@ pub fn trace_to_node(ops: &[*const u8]) -> Node {
         }
     }
 
-    loop {
+    while i < ops.len() {
         match next_op(ops, &mut i) {
             Op::LoadI8 => {
                 stack.push(Node::LitI8(next_oparg(ops, &mut i) as i8));
                 i += 2;
             }
             Op::Ret => {
-                res = stack.pop().unwrap();
+                // Call / Ret should be handled in two flavors:
+                // - A ret paired with a call can be inlined
+                // - An unpaired call or ret means recursive activation
+                //   and should be treated differently.
+                let top = stack.pop().unwrap();
                 let nlocals = next_oparg(ops, &mut i) as usize;
-                assert_eq!(stack.len(), nlocals);
-                break;
+                for _ in 0..nlocals {
+                    stack.pop();
+                }
+                stmts.push(Stmt::Ret(top, nlocals as u8));
             }
             Op::Add => {
                 let r = stack.pop().unwrap();
@@ -75,5 +84,5 @@ pub fn trace_to_node(ops: &[*const u8]) -> Node {
             }
         }
     }
-    res
+    Stmt::Seq(stmts)
 }
